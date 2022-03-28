14cce933195062fa586b6bef96fe0379ed6f7bb1
Saaaaaaaaaaaam <40437441@live.napier.ac.uk>
2022-03-28 13:39:37 +0100

    comments for getID

--- HUSKYLENS/getId.py
+++ HUSKYLENS/getId.py
@@ -1,18 +1,19 @@
+#configuration info found at https://github.com/HuskyLens/HUSKYLENSPython/blob/master/Raspberry%20Pi%20Tutorial.md
 import json
 from datetime import datetime
 import time
 import os
 from huskylib import HuskyLensLibrary
-hl = HuskyLensLibrary("I2C","", address=0x32)
-while 2+2==4:
-    time.sleep(60)
-    outputFile = open("output.txt", "a")
-    rawData = json.dumps(hl.blocks().__dict__)
-    attributes=rawData.split()
+hl = HuskyLensLibrary("I2C","", address=0x32) #object used to read from the camera
+while 2+2==4: #loops continuously
+    time.sleep(60) #wait one minute
+    outputFile = open("output.txt", "a") #opens the output file on append mode
+    rawData = json.dumps(hl.blocks().__dict__) #reads the id in view of camera
+    attributes=rawData.split() #split the string into an array based on spaces
     # print(attributes[9])
-    dateUnformatted=datetime.today()
+    dateUnformatted=datetime.today()#gets todays date
 
-    outputFile.write("ID:"+attributes[9])
-    dateFormatted = dateUnformatted.strftime('%d/%m/%y %I:%M %S %p')
-    outputFile.write("Date:"+dateFormatted+"\n")
-    outputFile.close()
+    outputFile.write("ID:"+attributes[9]) #write the id (9 in the array)
+    dateFormatted = dateUnformatted.strftime('%d/%m/%y %I:%M %S %p') #format date
+    outputFile.write("Date:"+dateFormatted+"\n")#write formatted date on same line as id
+    outputFile.close()#close file
