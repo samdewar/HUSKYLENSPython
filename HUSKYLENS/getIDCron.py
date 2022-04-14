956f13404c498e7d409ed8467275aac81adf0d7b
Saaaaaaaaaaaam <40437441@live.napier.ac.uk>
2022-04-14 12:10:26 +0100

    example of crontab scripts added

--- HUSKYLENS/getIDCron.py
+++ HUSKYLENS/getIDCron.py
@@ -4,15 +4,17 @@ import json
 from datetime import datetime
 import time
 import os
+
 from huskylib import HuskyLensLibrary
 hl = HuskyLensLibrary("I2C","", address=0x32)
 outputFile = open("birds.csv", "a")#opens the output file on append mode
+
 rawData = json.dumps(hl.blocks().__dict__)#reads the id in view of camera
 attributes=rawData.split()#split the string into an array based on spaces
-# print(attributes[9])
-dateUnformatted=datetime.today()#gets todays date
-
 outputFile.write("ID:"+attributes[9])#write the id (9 in the array)
+
+dateUnformatted=datetime.today()#gets todays date
 dateFormatted = dateUnformatted.strftime('%d/%m/%y %I:%M')#format date
 outputFile.write("Date:"+dateFormatted+"\n")#write formatted date on same line as id
+
 outputFile.close()#close file
