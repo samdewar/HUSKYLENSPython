29f3ead24214c06d5c9caeb7048c00b4cca75942
paulcrichton <78025872+paulcrichton@users.noreply.github.com>
2022-04-27 10:44:42 +0100

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -1,15 +1,18 @@
 import paho.mqtt.client as mqtt
+import matplotlib.pyplot as plt
 import time
 
 #variables
 broker_domain="127.0.0.1"
 
+	
 #decode message
 def on_message(client, userdata, msg):
         global message
         message = msg.payload
         client.disconnect()
-        print(message)
+		print(message)
+
     
 #logging
 def on_log(client, userdata, level, buf):
@@ -29,18 +32,19 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe("birds/data")
         time.sleep(10)
 		 
-
+#
 def connect_and_subscribe(broker):
 		global connection
 		#Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
 		client = mqtt.Client("lions_gate")
+		
 		#start coonection attempt
 		client.connect(broker, port=1883, keepalive=60)
 		connection = True
 		
        
 		client.loop_start()        
-                
+
 		client.on_connect=on_connect #callback function
 		print("Connecting to ",broker)
        
@@ -52,9 +56,55 @@ def connect_and_subscribe(broker):
         
 		client.loop_forever()
 
+#format data into array
+def format_data(unformatted_message):
+	counter = 0
+	formatted_message = []
+	unformatted_message = unformatted_message.split('[')
+	
+	#skip first two blank lines
+	for field in unformatted_message:
+		if counter != 2:
+			counter = counter + 1
+			continue
+		
+		#remove extras
+		field = field.replace("],","")
+		field = field.replace("]","")
+		field = field.replace("\'","")
 
-6
+		#add tp array
+		formatted_message.append(field.split(","))
+		
+	return formatted_message
+
+#count birds and add to dictionary
+def count_birds(formatted_message):
+	count_dict = {}
+
+	for field in formatted_message:
+		if field[1] not in  count_dict:
+			count_dict[field[1]] = 1
+		else:
+			tmp = count_dict[field[1]] 
+			tmp = tmp + 1
+			count_dict[field[1]] = tmp
+		
+	return count_dict
+
+#graph counted birds
+def graph(counted_birds):
+	names = list(counted_birds.keys())
+	values = list(counted_birds.values())
+
+	plt.bar(range(len(counted_birds)), values, tick_label=names)
+	plt.show()
+	
+#main fucntion
 def main(broker):
 	connect_and_subscribe(broker)
-	
+	formatted_message = format_data(message)
+	counted_birds = count_birds(formatted_message)
+	graph(counted_birds)
+
 main(broker_domain)
